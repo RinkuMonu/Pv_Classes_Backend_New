8e6aaa9bd3f2d61de59d61751d81efa31f7662c2
Rahul Singh <rahuljsrasr1112@gmail.com>
2026-07-08 14:15:21 +0530

    cors

--- Index.js
+++ Index.js
@@ -49,6 +49,7 @@ const app = express();
 app.use(
   cors({
     origin: [
+      "http://localhost:3000",
       "http://localhost:3000", // React default
       "http://localhost:5173", // Vite default
       "http://127.0.0.1:5173",
